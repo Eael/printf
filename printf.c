8e3caaa4efd02d5a10ce61d7289f043c751c7d6a
Kwame <1kwameearl@gmail.com>
2023-09-18 14:58:28 -0700

    tester

---
+++ printf.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _printf - prints a string in a formatted way
+ * @format: string to print
+ * @...: variadic parameters
+ *
+ * Return: number of characters printed, -1 on error
+ */
+
+int _printf(const char *format, ...)
+{
+	int i = 0, count = 0;
+	va_list args;
+	int (*func_to_use)(va_list);
+
+	va_start(args, format);
+	if (format == NULL)
+		return (-1);
+	while (format[i])
+	{
+		if (format[i] != '%')
+		{
+			count += _putchar(format[i]);
+			i++;
+			continue;
+		}
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == ' ' || format[i + 1] == '\0')
+				return (-1);
+			i++;
+			func_to_use = get_handler(format[i]);
+
+			if (func_to_use)
+			{
+				count += func_to_use(args);
+				i++;
+				continue;
+			}
+			else
+			{
+				_putchar('%');
+				count++;
+				continue;
+			}
+		}
+	}
+	va_end(args);
+	return (count);
+}
