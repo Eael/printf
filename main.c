b67093cc99931fb213f429dc20e1eefc23e8e041
Kwame <1kwameearl@gmail.com>
2023-09-18 06:04:27 -0700

    main.c

--- main.c
+++ main.c
@@ -11,13 +11,15 @@ int main(void)
 {
     int len;
     int len2;
+    char c = 'a';
+    char str[] = "I live and love";
 
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
-    _printf("Length:[%d, %i]\n", len, len);
-    printf("Length:[%d, %i]\n", len2, len2);
-    _printf("Negative:[%d]\n", -762534);
-    printf("Negative:[%d]\n", -762534);
-
+    printf("Length:[%d, %i]\n", len, len2);
+    _printf("Negative:[%s]\n", str);
+    _printf("character %c\n", c);
+    _printf("percentage %% nd string %s \n", str);
+    
     return (0);
 }
